f96d267d35405447efe277c47fb0cd18eb23c0f3
WILLIAN(home) <willianoliveira.tec@gmail.com>
2020-04-28 15:32:54 -0300

    Update Folders

---
+++ GoBarber/frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import SingIn from './pages/SingIn/index';
+// import SingUp from './pages/SingUp/index';
+
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SingIn />
+    </AuthProvider>
+    <ToastContainer />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
---
+++ GoBarber/frontend/src/pages/SingIn/index.tsx
@@ -0,0 +1,81 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErrors from '../../utils/getValidationErros';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+interface SignInFromData {
+  email: string;
+  password: string;
+}
+
+const SingIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { singIn } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: SignInFromData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email valido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        singIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+        // disparar um toast
+      }
+    },
+    [singIn],
+  );
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} type="text" placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="ttrtr">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+export default SingIn;
